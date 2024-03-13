14a85500333781f857f556511feb669b37e662e7
chintriago <35647407+chintriago@users.noreply.github.com>
2024-03-12 20:12:26 -0700

    styling for active and hover on main top text link

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,22 @@
 // HERO INPUT
 const heroInput = document.getElementById('hero-input');
-const emailError = document.getElementById('email-error')
-const heroButton = document.getElementById('hero-button')
+const heroEmailError = document.getElementById('hero-email-error')
+const heroBtn = document.getElementById('hero-button')
 let inputValue;
 let emailCheck;
 
-heroButton.addEventListener('click', function (event) {
+heroBtn.addEventListener('click', function (event) {
     event.preventDefault();
     inputValue = heroInput.value;
     emailCheck = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
     if (inputValue.match(emailCheck)) {
         heroInput.classList.remove('outline-error');
-        emailError.classList.add('remove');
+        heroEmailError.classList.add('remove');
         alert('email successfully submitted!');
     } else {
         event.preventDefault();
         heroInput.classList.add('outline-error');
-        emailError.classList.remove('remove');
+        heroEmailError.classList.remove('remove');
     }
 });
 
@@ -36,6 +36,7 @@ mainBtn.addEventListener('click', function (event) {
         alert('email successfully submitted!');
     } else {
         event.preventDefault();
+        mainEmailError.style.color = 'white';
         mainInput.classList.add('outline-error');
         mainEmailError.classList.remove('remove');
     }
